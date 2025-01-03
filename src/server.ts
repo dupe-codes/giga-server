8b3d9d8b69626a759440d512955f926f51d2eda0
dupe <dupe@dupe.codes>
2025-01-03 18:54:45 -0500

    chore: remove annoying comments lol

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,12 @@ import cors from "cors"
 import express, { Express, Router, Response, Request } from "express"
 import path from 'path'
 
-/**
-* configure server
-*/
 const server: Express = express()
 const PORT: string | number = process.env.PORT || 4000
 
 server.use(cors())
 server.use(express.static(path.join(__dirname, '../client')))
 
-/**
-* set up API routes
-*/
 const router: Router = Router()
 
 const hello = async (req: Request, res: Response): Promise<void> => {
@@ -21,16 +15,11 @@ const hello = async (req: Request, res: Response): Promise<void> => {
 }
 
 router.get("/hello", hello)
-
 router.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'))
 })
-
 server.use(router)
 
-/**
-* start server
-*/
 server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 )
