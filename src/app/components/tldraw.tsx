2a8f9cb9fb1e926423de0d499f6f258a91ee44f9
dupe <dupe@dupe.codes>
2025-01-04 00:38:46 -0500

    chore: added some dev tooling

--- src/app/components/tldraw.tsx
+++ src/app/components/tldraw.tsx
@@ -1,73 +1,86 @@
-import { React, useCallback, useEffect, useState } from "react"
-import { TLEditorSnapshot, Tldraw, getSnapshot, loadSnapshot, useEditor } from 'tldraw'
-import 'tldraw/tldraw.css'
+import { React, useCallback, useEffect, useState } from "react";
+import {
+  TLEditorSnapshot,
+  Tldraw,
+  getSnapshot,
+  loadSnapshot,
+  useEditor,
+} from "tldraw";
+import "tldraw/tldraw.css";
 
 // TODO: update component to ask for snapshot file name and POST to server to save
 //       if already saved as snapshot, autopopulate file name as the existing
 //       also get list of existing snapshots from server, show in dropdown to load
 function SnapshotToolbar() {
-	const editor = useEditor()
+  const editor = useEditor();
 
-	const save = useCallback(() => {
-		const { document, session } = getSnapshot(editor.store)
-		localStorage.setItem('snapshot', JSON.stringify({ document, session }))
+  const save = useCallback(() => {
+    const { document, session } = getSnapshot(editor.store);
+    localStorage.setItem("snapshot", JSON.stringify({ document, session }));
+  }, [editor]);
 
-	}, [editor])
+  const load = useCallback(() => {
+    const snapshot = localStorage.getItem("snapshot");
+    if (!snapshot) return;
+    loadSnapshot(editor.store, JSON.parse(snapshot));
+  }, [editor]);
 
-	const load = useCallback(() => {
-		const snapshot = localStorage.getItem('snapshot')
-		if (!snapshot) return
-		loadSnapshot(editor.store, JSON.parse(snapshot))
-	}, [editor])
+  const [showCheckMark, setShowCheckMark] = useState(false);
+  useEffect(() => {
+    if (showCheckMark) {
+      const timeout = setTimeout(() => {
+        setShowCheckMark(false);
+      }, 1000);
+      return () => clearTimeout(timeout);
+    }
+    return;
+  });
 
-	const [showCheckMark, setShowCheckMark] = useState(false)
-	useEffect(() => {
-		if (showCheckMark) {
-			const timeout = setTimeout(() => {
-				setShowCheckMark(false)
-			}, 1000)
-			return () => clearTimeout(timeout)
-		}
-		return
-	})
-
-	return (
-		<div style={{ padding: 20, pointerEvents: 'all', display: 'flex', gap: '10px' }}>
-			<span
-				style={{
-					display: 'inline-block',
-					transition: 'transform 0.2s ease, opacity 0.2s ease',
-					transform: showCheckMark ? `scale(1)` : `scale(0.5)`,
-					opacity: showCheckMark ? 1 : 0,
-				}}
-			>
-				Saved ✅
-			</span>
-			<button
-				onClick={() => {
-					save()
-					setShowCheckMark(true)
-				}}
-			>
-				Save Snapshot
-			</button>
-			<button onClick={load}>Load Snapshot</button>
-		</div>
-	)
+  return (
+    <div
+      style={{
+        padding: 20,
+        pointerEvents: "all",
+        display: "flex",
+        gap: "10px",
+      }}
+    >
+      <span
+        style={{
+          display: "inline-block",
+          transition: "transform 0.2s ease, opacity 0.2s ease",
+          transform: showCheckMark ? `scale(1)` : `scale(0.5)`,
+          opacity: showCheckMark ? 1 : 0,
+        }}
+      >
+        Saved ✅
+      </span>
+      <button
+        onClick={() => {
+          save();
+          setShowCheckMark(true);
+        }}
+      >
+        Save Snapshot
+      </button>
+      <button onClick={load}>Load Snapshot</button>
+    </div>
+  );
 }
 
-type Props = {}
+type Props = {};
 
-const Drawing : React.FC<Props> = ({}) => {
-    return (
-        <div style={{ position: 'fixed', inset: 0 }}>
-            <Tldraw
-              persistenceKey="giga"
-              components={{
-			    SharePanel: SnapshotToolbar,
-			  }} />
-		</div>
-    )
-}
+const Drawing: React.FC<Props> = ({}) => {
+  return (
+    <div style={{ position: "fixed", inset: 0 }}>
+      <Tldraw
+        persistenceKey="giga"
+        components={{
+          SharePanel: SnapshotToolbar,
+        }}
+      />
+    </div>
+  );
+};
 
-export default Drawing
+export default Drawing;
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,28 @@
-import cors from "cors"
-import express, { Express, Router, Response, Request } from "express"
-import path from 'path'
+import cors from "cors";
+import express, { Express, Router, Response, Request } from "express";
+import path from "path";
 
-const server: Express = express()
-const PORT: string | number = process.env.PORT || 4000
+const server: Express = express();
+const PORT: string | number = process.env.PORT || 4000;
 
-server.use(cors())
-server.use(express.static(path.join(__dirname, '../client')))
+// TODO: read config file from XDG_CONFIG/giga-server
+//       save drawings in config.drawings_dir
 
-const router: Router = Router()
+server.use(cors());
+server.use(express.static(path.join(__dirname, "../client")));
+
+const router: Router = Router();
 
 const hello = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json({ "message": "hello, world!" })
-}
+  res.status(200).json({ message: "hello, world!" });
+};
 
-router.get("/hello", hello)
-router.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'))
-})
-server.use(router)
+router.get("/hello", hello);
+router.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
+});
+server.use(router);
 
 server.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-)
+  console.log(`Server running on http://localhost:${PORT}`),
+);
